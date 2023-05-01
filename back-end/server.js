faebb221a1004469ce29a9621d75eb5de6155262
Liza-Diata <elise.diata15@gmail.com>
2023-05-02 00:38:47 +0800

    updated

--- back-end/server.js
+++ back-end/server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 
 
 app.get('/', (req, res) => {
-    res.send("API is running");
+    res.send("API is running Successfully");
 });
 
 app.use('/api/user', userRoutes);
@@ -28,24 +28,6 @@ app.use('/api/message', messageRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-
-// --------------------------deployment------------------------------
-
-const __dirname1 = path.resolve();
-
-if (process.env.NODE_ENV === "development") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
-
-// --------------------------deployment------------------------------
 
 // Error Handling middlewares
 app.use(notFound);
